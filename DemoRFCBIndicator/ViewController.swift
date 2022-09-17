95c237c7f21ad4d3fe77b771a2fbd50cb119a028
Rifat Firdaus <rifatkun@gmail.com>
2022-09-18 06:14:47 +0700

    Initial Project

---
+++ DemoRFCBIndicator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoRFCBIndicator
+//
+//  Created by Rifat Firdaus on 18/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RFCircularBounceIndicator/RFCircularBounceIndicator.h
@@ -0,0 +1,18 @@
+//
+//  RFCircularBounceIndicator.h
+//  RFCircularBounceIndicator
+//
+//  Created by Rifat Firdaus on 18/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RFCircularBounceIndicator.
+FOUNDATION_EXPORT double RFCircularBounceIndicatorVersionNumber;
+
+//! Project version string for RFCircularBounceIndicator.
+FOUNDATION_EXPORT const unsigned char RFCircularBounceIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RFCircularBounceIndicator/PublicHeader.h>
+
+
